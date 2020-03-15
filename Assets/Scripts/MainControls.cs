c0a2b78d1395dc016eaa5cf43cdb728a17f11458
mladsie deti <ma@itstep.lan>
2020-03-15 14:46:36 +0300

    Progect update

---
+++ Assets/Scripts/MainControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainControls : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+}
