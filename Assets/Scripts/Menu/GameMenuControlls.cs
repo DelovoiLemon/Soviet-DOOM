bdc1b38e0caae98bfe7a4696aec0a90080c981fd
DelovoiLemon <DelovoiLemon@github.com>
2020-03-26 12:21:03 +0300

    Map 2nd fix and added game menu

--- Assets/Scripts/Menu/GameMenuControlls.cs
+++ Assets/Scripts/Menu/GameMenuControlls.cs
@@ -6,19 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class GameMenuControlls : MonoBehaviour
 {
+    public GameObject menu;
+    bool state = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+         //= new GameObject("GameMenu");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey (KeyCode.Escape))
-            SceneManager.LoadScene("Menu");
-            
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
+        if(Input.GetKey (KeyCode.Escape)) state = true;
+        if(state)
+        {
+            menu.transform.gameObject.SetActive(true);
+            if (Input.GetKey (KeyCode.Y))
+            {
+                state = false;
+                menu.transform.gameObject.SetActive(false);
+                SceneManager.LoadScene("Menu");
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;   
+            }
+            else if (Input.GetKey (KeyCode.N))
+            {
+                state = false;
+                menu.transform.gameObject.SetActive(false);
+            }
+        }
     }
 }
